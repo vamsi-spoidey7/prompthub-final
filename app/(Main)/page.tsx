ce54fdafa867207539cddb6192e3a7a2ca79d93e
vamsi-spoidey7 <vamsimadugula7@gmail.com>
2024-01-03 13:29:43 +0530

    hero section modified

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Route/Hero";
+import Image from "next/image";
 
 type Props = {};
 const Page = (props: Props) => {
@@ -9,6 +10,16 @@ const Page = (props: Props) => {
                 <Header activeItem={0} />
                 <Hero />
             </div>
+            <Image
+                src={
+                    "https://pixner.net/aikeu/assets/images/footer/shape-two.png"
+                }
+                alt="star"
+                width={120}
+                height={120}
+                className="absolute right-0"
+            />
+            <br />
         </div>
     );
 };
--- components/Route/Hero.tsx
+++ components/Route/Hero.tsx
@@ -42,7 +42,7 @@ const rowTwoImages = [
 
 const Hero = (props: Props) => {
     return (
-        <div className="w-full md:min-h-screen flex items-center justify-center">
+        <div className="w-full md:min-h-screen flex items-center justify-center overflow-hidden">
             <div>
                 <h1 className="font-Montserrat text-4xl py-5 xl:text-7xl 2xl:text-8xl font-[700] text-center xl:leading-[80px] 2xl:leading-[100px] md:mt-20">
                     Make <span className="text-[#64ff4b]">Ai Image</span> <br />
