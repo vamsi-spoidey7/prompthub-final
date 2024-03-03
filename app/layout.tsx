be3acff7596e9ef5d200e48dcc18015f29837ae7
vamsi-spoidey7 <vamsimadugula7@gmail.com>
2024-03-03 15:14:26 +0530

    clerk setup added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
@@ -21,10 +22,12 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <body className={`${inter.variable} ${montserrat.variable}`}>
-                <NextUiProvider>{children}</NextUiProvider>
-            </body>
-        </html>
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.variable} ${montserrat.variable}`}>
+                    <NextUiProvider>{children}</NextUiProvider>
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
