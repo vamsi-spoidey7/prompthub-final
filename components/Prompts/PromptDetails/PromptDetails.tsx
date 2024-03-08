e0ccaae6d7a6a3fac2002b5060dc585416f69f4e
vamsi-spoidey7 <vamsimadugula7@gmail.com>
2024-03-08 14:22:00 +0530

    fixed related prompts

--- components/Prompts/PromptDetails/PromptDetails.tsx
+++ components/Prompts/PromptDetails/PromptDetails.tsx
@@ -4,13 +4,14 @@ import SellersBanner from "@/components/Shop/SellersBanner";
 import { prompt } from "@/@types/promptTypes";
 import { useEffect, useState } from "react";
 import PromptInformation from "./PromptInformation";
+import PromptCard from "../PromptCard";
+import PromptCardLoader from "@/utils/PromptCardLoader";
 
 type Props = {};
 
 const PromptDetails = ({ promptData }: { promptData: prompt | undefined }) => {
     const [prompts, setPrompts] = useState<prompt[]>();
     const [loading, setLoading] = useState(true);
-
     useEffect(() => {
         const fetchPromptData = async () => {
             setLoading(true);
@@ -19,7 +20,10 @@ const PromptDetails = ({ promptData }: { promptData: prompt | undefined }) => {
                     `/api/get-related-prompts?promptCategory=${promptData?.category}`
                 );
                 const data = await response.json();
-                setPrompts(data);
+                const relatedPrompts = data.filter(
+                    (prompt: prompt) => prompt.id !== promptData?.id
+                );
+                setPrompts(relatedPrompts);
             } catch (error) {
                 console.error("Failed to fetch prompts:", error);
             } finally {
@@ -38,7 +42,27 @@ const PromptDetails = ({ promptData }: { promptData: prompt | undefined }) => {
             <PromptInformation promptData={promptData} />
             <br />
             <h1 className={`${styles.heading} px-2 pb-2`}>Related Prompts</h1>
-            <div className="flex flex-wrap">{/* Prompt Card  */}</div>
+            <div className="flex flex-wrap">
+                {loading ? (
+                    [...new Array(4)].map((i) => (
+                        <>
+                            <PromptCardLoader />
+                        </>
+                    ))
+                ) : (
+                    <>
+                        {prompts &&
+                            prompts.map((item: any) => (
+                                <PromptCard prompt={item} key={item.id} />
+                            ))}
+                    </>
+                )}
+            </div>
+            {prompts?.length === 0 && (
+                <p className={`${styles.label} text-center block my-5`}>
+                    No prompt found with this category!
+                </p>
+            )}
             <br />
             <br />
             <SellersBanner />
