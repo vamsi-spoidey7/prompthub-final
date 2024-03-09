c36fdfd825015cfb3dbe1398657948b15d85edab
vamsi-spoidey7 <vamsimadugula7@gmail.com>
2024-03-09 20:09:03 +0530

    get top sellers dynamically

--- components/Shop/SellerCard.tsx
+++ components/Shop/SellerCard.tsx
@@ -1,23 +1,39 @@
 import Ratings from "@/utils/Ratings";
 import { styles } from "@/utils/styles";
-import { Avatar, Card } from "@nextui-org/react";
+import { Avatar, Card, Skeleton } from "@nextui-org/react";
 
-type Props = {};
-const SellerCard = (props: Props) => {
+type Props = {
+    item?: any;
+    loading: boolean;
+};
+const SellerCard = ({ item, loading }: Props) => {
     return (
         <Card className="py-4 bg-[#100d21] m-3 w-full md:w-[31%] 2xl:w-[23%] cursor-pointer flex flex-col items-center text-white border border-[#ffffff22]">
-            <Avatar
-                src="https://pixner.net/aikeu/assets/images/blog-details/a-one.png"
-                className="w-[80px] h-[80px]"
-            />
-            <span className={`${styles.label} py-2 text-xl`}>
-                @VamsiMadugula
-            </span>
-            <div className="flex items-center">
-                <span className={`${styles.label} pr-2`}>4.5/5</span>
-                <Ratings rating={4.5} />
-            </div>
-            <span className={`${styles.label} py-2`}>Total Sales: 512</span>
+            {loading ? (
+                <>
+                    <Skeleton className="w-[80px] h-[80px] rounded-full" />
+                    <br />
+                    <Skeleton className="w-[90%] rounded-xl h-[20px]" />
+                    <br />
+                    <Skeleton className="w-[90%] rounded-xl h-[20px]" />
+                </>
+            ) : (
+                <>
+                    <Avatar src={item?.avatar} className="w-[80px] h-[80px]" />
+                    <span className={`${styles.label} py-2 text-xl`}>
+                        @{item?.name}
+                    </span>
+                    <div className="flex items-center">
+                        <span className={`${styles.label} pr-2`}>
+                            {item?.ratings}/5
+                        </span>
+                        <Ratings rating={item?.ratings} />
+                    </div>
+                    <span className={`${styles.label} py-2`}>
+                        Total Sales: {item?.allProducts}
+                    </span>
+                </>
+            )}
         </Card>
     );
 };
