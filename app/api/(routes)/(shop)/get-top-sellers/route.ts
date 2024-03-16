060996046738a5eb10224ff0694a0c98e95ad65e
vamsi-spoidey7 <vamsimadugula7@gmail.com>
2024-03-16 13:06:55 +0530

    top sellers static data issue solved

--- app/api/(routes)/(shop)/get-top-sellers/route.ts
+++ app/api/(routes)/(shop)/get-top-sellers/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prismaDb";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest) {
     try {
         const sellers = await prisma.shops.findMany({
